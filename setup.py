6636c15b352d8c5066981e4a7f3608994aaf613f
Klaas Boesche <boesche@cs.uni-saarland.de>
2013-07-07 15:24:52 +0200

    added first version of setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='python-dmenuwrap',
+    author='Klaas Boesche',
+    author_email='klaas-dev@boesche.me',
+    url='https://github.com/KaGeBe/python-dmenuwrap',
+    version='0.1.0',
+    license='BSD 2-clause',
+    py_modules=['dmenuwrap']
+)
